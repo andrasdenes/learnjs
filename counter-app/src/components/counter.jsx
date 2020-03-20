6c334dc49c047b0f7d2a3aea2c27a5d84972de53
SASMob-FCH-17 <sasmob-fch-17@inclouded.hu>
2020-03-20 18:48:30 +0100

    untilDebug1-28-02

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,24 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: []
   };
 
   handleIncrement = () => {
-    console.log("Increment clicked", this);
+    this.setState({value: this.state.value + 1})
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
 
   render() {
     return (
@@ -31,20 +20,19 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {this.renderTags()}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value <= 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value: count } = this.state;
+    return count <= 0 ? "Zero" : count;
   }
 }
 
