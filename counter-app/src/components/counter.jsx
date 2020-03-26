cec3dc758ad15096776fa1918fec1beae74bcdc4
SASMob-FCH-17 <sasmob-fch-17@inclouded.hu>
2020-03-26 11:28:48 +0100

    mosh-end, self-start

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps);
+    console.log(prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+      //AJAX CALL, get new data??
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("counter - unmount");
+  }
 
   render() {
     return (
@@ -12,9 +23,10 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <button 
-        onClick={() => this.props.onDelete(this.props.counter.id)} 
-        className="btn btn-danger btn-sm m-2">
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
           Delete
         </button>
       </div>
